725c9542d2ba3739c9cad89a812a38addc225311
Gene Culling <culling66@gmail.com>
2022-04-17 09:14:56 +1200

    Add exercisetracker velocitymacro

--- src/main/java/com/geneculling/javakata/servlet/ExerciseTrackerServlet.java
+++ src/main/java/com/geneculling/javakata/servlet/ExerciseTrackerServlet.java
@@ -1,16 +1,30 @@
 package com.geneculling.javakata.servlet;
 
+import com.atlassian.templaterenderer.TemplateRenderer;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExerciseTrackerServlet extends HttpServlet {
+    TemplateRenderer renderer;
 
+    ExerciseTrackerServlet(@ComponentImport TemplateRenderer renderer){
+        this.renderer = renderer;
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().write("Hello World");
+        response.setContentType("text/html");
+
+        Map<String, Object> map = new HashMap<>();
+
+        renderer.render("templates/exercisetracker.vm", map, response.getWriter());
+        response.flushBuffer();
     }
 
 }
